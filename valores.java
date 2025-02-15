3cdba9bb87ed72e1ac901bfbf8b7f86a1ddb3b7f
BlasGamer27 <urielblas86@gmail.com>
2025-02-14 19:45:34 -0600

    Add files via upload

---
+++ valores.java
@@ -0,0 +1,17 @@
+//Definición de la clase:
+public class valores{
+	//Definición del método main:
+	public static void main(String[] args) {
+        // Definición de los valores
+        int valor1 = 10;
+        int valor2 = 5;
+        String c = "Este es el valor de c";
+
+        // Comparación de los valores
+        if (valor1 > valor2) {
+            System.out.println("holi esto es valor más alto: " + valor1);
+        } else {
+            System.out.println(c);
+        }
+    }
+}
